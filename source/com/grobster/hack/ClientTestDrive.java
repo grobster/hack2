c9b5ea2e4dab866e0d6236fe53667c186b9842e1
grobster <jquarles@grobster.com>
2015-05-30 15:46:59 -0400

    changed starting path in ClientTestDrive class

--- source/com/grobster/hack/ClientTestDrive.java
+++ source/com/grobster/hack/ClientTestDrive.java
@@ -4,7 +4,7 @@ import java.nio.file.*;
 
 public class ClientTestDrive {
 	public static void main(String[] args) {
-		Path startingPath = Paths.get("C:\\");
+		Path startingPath = Paths.get("C:\\Users\\");
 		FileFinder ff = new FileFinder(startingPath);
 		ff.search();
 		Client client = new Client();
