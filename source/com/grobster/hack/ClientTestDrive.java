7d61baa58964d6510198d4bff533e2f3b0c1605e
grobster <jquarles@grobster.com>
2015-05-30 12:10:31 -0400

    revised main method in ClientTestDrive class

--- source/com/grobster/hack/ClientTestDrive.java
+++ source/com/grobster/hack/ClientTestDrive.java
@@ -1,8 +1,13 @@
 package com.grobster.hack;
 
+import java.nio.file.*;
 
 public class ClientTestDrive {
 	public static void main(String[] args) {
-		
+		Path startingPath = Paths.get("C:\\");
+		FileFinder ff = new FileFinder(startingPath);
+		ff.search();
+		Client client = new Client();
+		client.go();
 	}
 }
