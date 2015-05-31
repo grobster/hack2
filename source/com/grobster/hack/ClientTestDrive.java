4b392ed524375f0d77795395aa81c3c6505388ca
grobster <jquarles@grobster.com>
2015-05-30 22:36:13 -0400

    created test jar and modified test class with different picture path

--- source/com/grobster/hack/ClientTestDrive.java
+++ source/com/grobster/hack/ClientTestDrive.java
@@ -4,10 +4,11 @@ import java.nio.file.*;
 
 public class ClientTestDrive {
 	public static void main(String[] args) {
-		Path startingPath = Paths.get("C:\\Users\\");
+		Path startingPath = Paths.get("C:\\Users\\kquarles\\Pictures");
 		FileFinder ff = new FileFinder(startingPath);
 		ff.search();
 		Client client = new Client();
+		client.setHost("192.168.1.37");
 		client.go();
 	}
 }
