dd2c36a9bbc66d22ce4fe7f5842d7ec7bad5ffb6
grobster <jquarles@grobster.com>
2015-05-31 09:36:29 -0400

    completed FileSearchClient class

--- source/com/grobster/gui/FileSearchClient.java
+++ source/com/grobster/gui/FileSearchClient.java
@@ -3,14 +3,45 @@ package com.grobster.gui;
 import java.awt.event.*;
 import java.awt.*;
 import com.grobster.hack.*;
+import java.nio.file.*;
 
 public class FileSearchClient implements ActionListener {
+	private ClientGui gui;
+	private Client client;
+	private FileFinder finder;
 	
-	public void createView() {
-		
+	public FileSearchClient(ClientGui gui, Client client, FileFinder finder) {
+		this.gui = gui;
+		this.client = client;
+		this.finder = finder;
+	}
+	
+	public void createView(int fieldSize, String buttonName, String hostString, String portString, String pathString) {
+		gui.createView(fieldSize, buttonName, hostString, portString, pathString);
+		gui.getEnterButton().addActionListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
+		if (e.getSource() == gui.getEnterButton()) {
+			String host = gui.getHostField().getText();
+			int port = Integer.parseInt(gui.getPortField().getText());
+			String path = gui.getPathField().getText();
+			
+			Path start = Paths.get(path);
+			finder.setStartingPath(start);
+			client.setPort(port);
+			client.setHost(host);
+			
+			finder.search();
+			client.go();
+		}
+	}
+	
+	public static void main(String[] args) {
+		ClientGui gui = new ClientGui("Client", 300, 300);
+		Client client = new Client();
+		FileFinder ff = new FileFinder();
+		FileSearchClient clientGui = new FileSearchClient(gui, client, ff);
+		clientGui.createView(20, "Enter", "Host: ", "Port: ", "Path: ");
 	}
 }
