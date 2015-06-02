758200a79f3d5814ed53f55431f880c43a825770
grobster <jquarles@grobster.com>
2015-06-02 11:51:11 -0400

    added all of the exceptions thrown by Files.copy() method

--- source/com/grobster/hack/FileFinder.java
+++ source/com/grobster/hack/FileFinder.java
@@ -51,14 +51,26 @@ public class FileFinder {
 		@Override
 		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
 			if (predicate.test(file)) {
+				Path target = toServerPath.resolve(file.getFileName());
+				
 				try {
-					Path target = toServerPath.resolve(file.getFileName());
 					Files.copy(file, target);
-					System.out.println(file.getFileName());
-					foundFiles.add(file);
-				} catch (Exception ex) {
-					ex.printStackTrace();
+				} catch (UnsupportedOperationException ex) {
+					System.out.println("Operation not supported");
+				} catch (FileAlreadyExistsException ex) {
+					System.out.println("file already exists");
+				} catch (DirectoryNotEmptyException ex) {
+					System.out.println("Directory not empty");
+				} catch (AtomicMoveNotSupportedException ex) {
+					System.out.println("Atomic move operation failed");
+				} catch (IOException ex) {
+					System.out.println("IO operation failed");
+				} catch (SecurityException ex) {
+					System.out.println("security prevents access to source and target");
 				}
+				
+				System.out.println(file.getFileName());
+				foundFiles.add(file);
 			}
 			return FileVisitResult.CONTINUE;
 		}
